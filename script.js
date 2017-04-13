5b5be9149f1eea64ce098f5eafb33cf9642e9c68
nigelsingh27 <nigel.singh27@gmail.com>
2017-04-13 00:32:06 -0400

    final css, script.js, and html files

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function modify_qty(val) {
     document.getElementById('qty').value = new_qty;
     return new_qty;
 }
+function modify_qty1(val1) {
+    var qty = document.getElementById('qty1').value;
+    var new_qty = parseInt(qty,10) + val1;
+    
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+	if (new_qty > 5){
+		new_qty = 5;
+	}
+    
+    document.getElementById('qty1').value = new_qty;
+    return new_qty;
+}
